ecd0d20f9ab9eef0cd8b5351ad7d563d17993ff0
Alumno <Alumno@LAB1504-22>
2024-06-15 12:56:00 -0500

    Agregar archivos de proyecto.

---
+++ Lab13/Controllers/CustomerController.cs
@@ -0,0 +1,98 @@
+﻿using Lab13.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CustomerController : ControllerBase
+{
+    private readonly ApplicationDbContext _context;
+
+    public CustomerController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    // GET: api/Customer
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Customer>>> GetCustomers()
+    {
+        return await _context.Customers.Where(c => !c.IsDeleted).ToListAsync();
+    }
+
+    // GET: api/Customer/5
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Customer>> GetCustomer(int id)
+    {
+        var customer = await _context.Customers.FindAsync(id);
+
+        if (customer == null || customer.IsDeleted)
+        {
+            return NotFound();
+        }
+
+        return customer;
+    }
+
+    // PUT: api/Customer/5
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutCustomer(int id, Customer customer)
+    {
+        if (id != customer.IdCustomers)
+        {
+            return BadRequest();
+        }
+
+        _context.Entry(customer).State = EntityState.Modified;
+
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!CustomerExists(id))
+            {
+                return NotFound();
+            }
+            else
+            {
+                throw;
+            }
+        }
+
+        return NoContent();
+    }
+
+    // POST: api/Customer
+    [HttpPost]
+    public async Task<ActionResult<Customer>> PostCustomer(Customer customer)
+    {
+        _context.Customers.Add(customer);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction("GetCustomer", new { id = customer.IdCustomers }, customer);
+    }
+
+    // DELETE: api/Customer/5
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteCustomer(int id)
+    {
+        var customer = await _context.Customers.FindAsync(id);
+        if (customer == null)
+        {
+            return NotFound();
+        }
+
+        customer.IsDeleted = true; 
+        _context.Entry(customer).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    private bool CustomerExists(int id)
+    {
+        return _context.Customers.Any(e => e.IdCustomers == id);
+    }
+}
